05a9a014bb15aac6979dc887086a8dba361865d0
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-03-03 14:13:51 -0500

    Add a stub VPC global module

---
+++ modules/global/vpcs/inputs.tf
@@ -0,0 +1,7 @@
+variable aws_profile {}
+variable aws_regions {}
+variable environments {}
+variable account_name {}
+variable nubis_version {}
+variable environments_networks {}
+variable environments_ipsec_targets {}
