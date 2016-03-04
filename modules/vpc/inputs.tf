78f008adf85579f3f6f31471dc899b6508675cc5
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-03-04 12:56:44 -0500

    parametrize the nubis ssh key

--- modules/vpc/inputs.tf
+++ modules/vpc/inputs.tf
@@ -11,6 +11,10 @@ variable stage_ipsec_target {}
 variable account_name {}
 variable environments {}
 
+variable nubis_ssh_key {
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC0/tR0k8b6gIQpd6IHyEJdzmGur60ShmOdQGpBoF7IPMBWTHgc5w3CTcqvK6aJ6GpZHyybi9D9EON4+1WZTf9tcsdUP8kyVOs66sw26FWeCri2k1zomsGP9Ysr3bSUe3dpi5vipk1PDXpaD6wYs/eEtQxO1U1wRCGEGclRdh5G8UbOMwrPIHvQd77ma5RyXzd36htzFtsKnuyTtG7xHGPphzVqLZmiDZeyxbr3mCuaMBW30syEKviiVbMo4RsmDqzR3N2ltInGKYgZpCW7fd7KrZL/G0oi/XS+Up5MvmYSsP2tYNx909CWFpWDsXEPMNddl7ZYizHXLbLexU8+0h5j nubis"
+}
+
 variable technical_owner {
   default = "infra-aws@mozilla.com"
 }
@@ -25,3 +29,4 @@ variable ssh_key_name {
 variable enabled {
   default = 1
 }
+
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -6,11 +6,11 @@ provider "aws" {
 resource "aws_key_pair" "nubis" {
   count = "${var.enabled}"
   key_name = "nubis"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC0/tR0k8b6gIQpd6IHyEJdzmGur60ShmOdQGpBoF7IPMBWTHgc5w3CTcqvK6aJ6GpZHyybi9D9EON4+1WZTf9tcsdUP8kyVOs66sw26FWeCri2k1zomsGP9Ysr3bSUe3dpi5vipk1PDXpaD6wYs/eEtQxO1U1wRCGEGclRdh5G8UbOMwrPIHvQd77ma5RyXzd36htzFtsKnuyTtG7xHGPphzVqLZmiDZeyxbr3mCuaMBW30syEKviiVbMo4RsmDqzR3N2ltInGKYgZpCW7fd7KrZL/G0oi/XS+Up5MvmYSsP2tYNx909CWFpWDsXEPMNddl7ZYizHXLbLexU8+0h5j nubis"
+  public_key = "${var.nubis_ssh_key}"
 
- provisioner "local-exec" {
+  provisioner "local-exec" {
    command = "sleep 30"
- }
+  }
 }
 
 resource "tls_private_key" "default" {
