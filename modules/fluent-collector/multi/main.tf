d56d32e510dc6eb2cfc87c33600176c2dfd22f26
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-03-22 15:18:48 -0400

    add missing ELB account ids

--- modules/fluent-collector/multi/main.tf
+++ modules/fluent-collector/multi/main.tf
@@ -49,12 +49,17 @@ resource "aws_s3_bucket" "fluent" {
   }
 }
 
-
 variable "elb_account_ids" {
   default = {
-    us-east-1 = "127311923021"
-    us-west-1 = "027434742980"
-    us-west-2 = "797873946194"
+    us-east-1      = "127311923021"
+    us-west-1      = "027434742980"
+    us-west-2      = "797873946194"
+    eu-central-1   = "054676820928"
+    ap-southeast-1 = "114774131450"
+    ap-northeast-1 = "582318560864"
+    ap-southeast-2 = "783225319266"
+    ap-northeast-2 = "600734575887"
+    sa-east-1      = "507241528517"
   }
 }
 
