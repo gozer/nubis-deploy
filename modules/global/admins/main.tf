d3d9d3ba3f0d2b0b35804d826ba911757c491635
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-04-20 11:54:28 -0400

    Make admin users, well, Admins.
    Also, create one IAM Role for each admin user, handy for STS stuff

--- modules/global/admins/main.tf
+++ modules/global/admins/main.tf
@@ -21,6 +21,44 @@ resource "aws_iam_user" "admin" {
   name  = "${element(split(",",var.admin_users), count.index)}"
 }
 
+resource "aws_iam_role_policy" "admin" {
+    count = "${length(split(",",var.admin_users))}"
+    name = "${element(split(",",var.admin_users), count.index)}"
+
+    role = "${element(aws_iam_role.admin.*.id, count.index)}"
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "*",
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "admin" {
+    count = "${length(split(",",var.admin_users))}" 
+    path  = "/nubis/admin/"
+    name = "${element(split(",",var.admin_users), count.index)}"
+    assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal" : { "AWS" : "${element(aws_iam_user.admin.*.arn, count.index)}" },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_iam_access_key" "admins" {
   count = "${length(split(",",var.admin_users))}"
   user  = "${element(aws_iam_user.admin.*.name, count.index)}"
@@ -45,6 +83,12 @@ resource "aws_iam_policy_attachment" "read_only" {
   name       = "read-only-attachments"
   groups     = ["${aws_iam_group.read_only_users.name}"]
   policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
+
+resource "aws_iam_policy_attachment" "admins" {
+  name       = "admins"
+  groups     = ["${aws_iam_group.admins.name}"]
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
 resource "aws_iam_group_policy" "nacl_admins" {
