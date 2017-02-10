12e6c5a8fdacaf0a9d0314acff48f95119b9c3ed
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-02-10 10:07:42 -0500

    Enable Cloudtrail logfile validation
    Fixes #193

--- modules/global/opsec/main.tf
+++ modules/global/opsec/main.tf
@@ -11,6 +11,7 @@ resource "aws_cloudtrail" "opsec-cloudtrail" {
   is_multi_region_trail         = true
   include_global_service_events = true
   enable_logging                = true
+  enable_log_file_validation    = true
 }
 
 resource "aws_cloudformation_stack" "opsec" {
