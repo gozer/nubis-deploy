12d3bba677a0f7e8d3cf2c1324636fd62c003621
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-08-01 16:51:28 -0400

    Switch over to develop

--- modules/consul/main.tf
+++ modules/consul/main.tf
@@ -1,5 +1,5 @@
 module "consul" {
-  source = "github.com/nubisproject/nubis-consul//nubis/terraform/multi?ref=v1.5.0"
+  source = "github.com/nubisproject/nubis-consul//nubis/terraform/multi?ref=develop"
 
   enabled = "${var.enabled}"
 
