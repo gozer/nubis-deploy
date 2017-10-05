fdab68155db462e0eebcce1f56e946b5088cf467
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-10-05 10:21:01 -0400

    enabled_metrics++

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -633,6 +633,17 @@ resource "aws_autoscaling_group" "nat" {
 
   launch_configuration = "${element(aws_launch_configuration.nat.*.name, count.index)}"
 
+  enabled_metrics = [
+    "GroupMinSize",
+    "GroupMaxSize",
+    "GroupDesiredCapacity",
+    "GroupInServiceInstances",
+    "GroupPendingInstances",
+    "GroupStandbyInstances",
+    "GroupTerminatingInstances",
+    "GroupTotalInstances",
+  ]
+
   tag {
     key                 = "Name"
     value               = "NAT (${var.nubis_version}) for ${var.account_name} in ${element(var.arenas, count.index/2)}/${lookup(var.nat_side,count.index%2)}"
@@ -750,7 +761,7 @@ resource "aws_iam_instance_profile" "nat" {
 }
 
 module "jumphost" {
-  source = "github.com/nubisproject/nubis-jumphost//nubis/terraform?ref=v1.6.0-dev"
+  source = "github.com/gozer/nubis-jumphost//nubis/terraform?ref=feature%2Farena"
 
   enabled = "${var.enabled * var.enable_jumphost}"
 
@@ -785,7 +796,7 @@ resource "aws_iam_role_policy_attachment" "fluent" {
 }
 
 module "fluent-collector" {
-  source = "github.com/nubisproject/nubis-fluent-collector//nubis/terraform?ref=v1.6.0-dev"
+  source = "github.com/gozer/nubis-fluent-collector//nubis/terraform?ref=feature%2Farena"
   
   enabled            = "${var.enabled * var.enable_fluent}"
   monitoring_enabled = "${var.enabled * var.enable_fluent * var.enable_monitoring}"
@@ -832,7 +843,7 @@ resource "aws_iam_role_policy_attachment" "monitoring" {
 }
 
 module "monitoring" {
-  source = "github.com/nubisproject/nubis-prometheus//nubis/terraform?ref=v1.6.0-dev"
+  source = "github.com/gozer/nubis-prometheus//nubis/terraform?ref=feature%2Farena"
 
   enabled = "${var.enabled * var.enable_monitoring}"
 
@@ -879,7 +890,7 @@ resource "aws_iam_role_policy_attachment" "sso" {
 }
 
 module "sso" {
-  source = "github.com/nubisproject/nubis-sso//nubis/terraform?ref=v1.6.0-dev"
+  source = "github.com/gozer/nubis-sso//nubis/terraform?ref=feature%2Farena"
   
   enabled = "${var.enabled * var.enable_sso}"
 
@@ -921,7 +932,7 @@ resource "aws_iam_role_policy_attachment" "consul" {
 }
 
 module "consul" {
-  source = "github.com/nubisproject/nubis-consul//nubis/terraform?ref=v1.6.0-dev"
+  source = "github.com/gozer/nubis-consul//nubis/terraform?ref=feature%2Farena"
 
   enabled = "${var.enabled * var.enable_consul}"
 
@@ -964,7 +975,7 @@ resource "aws_iam_role_policy_attachment" "ci" {
 
 # XXX: This assumes it's going in the first arena of the first region
 module "ci" {
-  source = "github.com/nubisproject/nubis-ci//nubis/terraform?ref=v1.6.0-dev"
+  source = "github.com/gozer/nubis-ci//nubis/terraform?ref=feature%2Farena"
 
   enabled = "${var.enabled * var.enable_ci * ((1 + signum(index(concat(split(",", var.aws_regions), list(var.aws_region)),var.aws_region))) % 2 )}"
 
