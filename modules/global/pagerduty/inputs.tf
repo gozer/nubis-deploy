38ffe5e1d965e1ea655f1270552232c107d941f1
Ed Lim <limed@sudoers.org>
2018-02-28 08:33:21 -0800

    Added pagerduty module support (#389)

---
+++ modules/global/pagerduty/inputs.tf
@@ -0,0 +1,13 @@
+variable "pagerduty_token" {}
+
+variable "enable_pagerduty" {}
+
+variable "pagerduty_team_name" {}
+
+variable "pagerduty_platform_critical_escalation_policy" {}
+
+variable "pagerduty_platform_non_critical_escalation_policy" {}
+
+variable "pagerduty_application_critical_escalation_policy" {}
+
+variable "pagerduty_application_non_critical_escalation_policy" {}
