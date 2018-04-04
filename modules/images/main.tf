b225eb81c6f577cb337df92eda9fd70560f73fa7
limed <limed@mozilla.com>
2018-04-04 14:57:03 -0700

    Restore images module

---
+++ modules/images/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  version = "~> 0.1"
+  region  = "${var.region}"
+}
+
+data "aws_ami" "image" {
+  most_recent = true
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+
+  filter {
+    name = "name"
+
+    values = [
+      "${var.project} ${var.version} ${var.storage} ${var.os}",
+    ]
+  }
+
+  owners = ["${var.owner}"]
+}
